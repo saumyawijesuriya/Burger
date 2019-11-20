0f738f0531b129e13ae9e60fe7e92775c676d8f0
Saumya Wijesuriya <Saumyawijesuriya@yahoo.com>
2019-11-19 22:01:02 -0500

    C to c

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -2,7 +2,7 @@ var express = require("express");
 
 var router = express.Router();
 
-var burger = require("../models/burger.js");
+var burger = require("../models/burger");
 
 router.get("/", function (req, res) {
   burger.all(function (data) {
